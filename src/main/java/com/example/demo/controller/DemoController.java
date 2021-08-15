351fcb0209c7d351a939f34795f3cbbc9fb28330
RaiReena10 <87465652+RaiReena10@users.noreply.github.com>
2021-08-15 10:45:28 +0530

    Update DemoController.java

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+// First Hello Controller
 @Controller
 public class DemoController {
 	
