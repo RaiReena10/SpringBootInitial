2488cf63705bef08959bb32225d8be1613b7a4c5
RaiReena10 <reena_manit@yahoo.co.in>
2021-08-16 15:06:03 +0530

    hotfix branch

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	// Added code in Master branch
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
