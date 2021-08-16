da837aea4f12f3eea8c3cc2ea427dc8f7f06052e
RaiReena10 <reena_manit@yahoo.co.in>
2021-08-16 14:52:45 +0530

    DemoApplication commited in master

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	// Added code in development branch
+	// Added code in Master branch
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
