e39dcd55d7abc33b737d4a09243fd72b65a7d5e3
RaiReena10 <reena_manit@yahoo.co.in>
2021-08-16 14:56:17 +0530

    Master branch

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,9 +3,6 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-// Development Branch  Hello Controller
-
-
 
 @Controller
 public class DemoController {	
